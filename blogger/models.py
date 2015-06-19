fa2a1ec5f9be5d94a9e33aba541547c0b95d43f0
fullmetal <abhinav23194@gmail.com>
2015-06-19 17:33:58 +0530

    initialisation complete, base user model and post model created

---
+++ blogger/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class user(models.Model):
+	## Enter table columns for user field
+	firstname=models.CharField(max_length=50)
+	lastname=models.CharField(max_length=50)
+	email=models.CharField(max_length=20)
+	password=models.CharField(max_length=8)
+
+	def __str__(self):
+		# shows the name of object in human readable format
+		return self.firstname
+
+
+class post(models.Model):
+	## Relates to posts , associated with user
+	user_post=models.ForeignKey(user)
+	post_title=models.CharField(max_length=50)
+	post_text=models.CharField(max_length=400)
+	post_update_time=models.DateTimeField('date updated')		## inside '' denotes explanation to administrator 
+	upvote=models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.post_title
